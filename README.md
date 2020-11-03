064296a007ed7a2ad5930932c2667a55c7739ad2
r4mdev <ajayram192@gmail.com>
2020-11-03 05:39:57 +0400

    Files updation5

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 <h1>GUESSING GAME</h1>
 Date & Time: 
-Tue Nov  3 05:06:12 AM +04 2020
+Tue Nov  3 05:37:22 AM +04 2020
 <br/>No of lines of code: 
-17
+37
--- guessinggame.sh
+++ guessinggame.sh
@@ -1,17 +1,36 @@
-number=$(ls -l | wc -l)
-while true
-do
-	echo "Enter the number of files(whole numbers only):"
-	read response
-	if [[ $response -lt $number ]]
-	then
-		echo "Wrong!! Entry too low, try again."
-	elif [[ $response -gt $number ]]
-	then
-		echo "Wrong!! Entry too high, try again."
-	elif [[ $response -eq $number ]]
-	then
-		echo "Congrtulations!! Right entry."
-		break
-	fi
-done
+# File: guessinggame.sh
+
+# this program allows the user to guess the number of files in the current directory until the correct answer is given
+
+# define function
+function game {
+
+# loop while user gives incorrect answer
+	while true
+
+		do
+			# prompt user for number of files
+			echo "Enter the number of files(whole numbers only):"
+
+			# accepting response from user
+			read response
+
+			if [[ $response -lt $number ]]
+			then
+				echo "Wrong!! Entry too low, try again."
+
+			elif [[ $response -gt $number ]]
+			then
+				echo "Wrong!! Entry too high, try again."
+
+			elif [[ $response -eq $number ]]
+			then
+				echo "Congrtulations!! Right entry."
+				break
+
+			fi
+		done
+}
+
+# call function
+game
